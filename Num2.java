acea4a8488c02600125ac6b0dba51feeb356fa18
Romario477 <60476563+Romario477@users.noreply.github.com>
2021-04-17 01:13:52 -0400

    Add files via upload

---
+++ Num2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Num2{
+   public static void main(String []args){
+      int y=0;
+      for(int i=0; i<20;i++){
+         x+=1;
+         y+=1;
+      }
+      System.out.println("X= "+x);
+      System.out.println("Y= "+y);
+   }
+}
